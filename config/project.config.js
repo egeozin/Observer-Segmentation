9ad61645fc59909b7d399fdac751249b59f335ac
egeozin <egeozin@mit.edu>
2017-12-01 16:29:14 -0500

    finals_20

--- config/project.config.js
+++ config/project.config.js
@@ -25,7 +25,7 @@ const config = {
   // Server Configuration
   // ----------------------------------
   //server_host : ip.address(), // use string 'localhlost' to prevent exposure on local network
-  server_host: '' || 'localhost' || ip.address()
+  server_host: process.env.IP
   server_port : process.env.PORT || 3000,
 
   // ----------------------------------
